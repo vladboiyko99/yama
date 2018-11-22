d843512e0566cf02c7214518237eb61c63faef65
vaif <vaif@inbox.ru>
2018-11-22 23:22:10 +0300

    'add'

--- index.php
+++ index.php
@@ -48,15 +48,40 @@ include "creat_ob.php";
 	<? endif ; ?>
    </div>
     
-  <div class = "form">
-	<form method = 'POST' action = 'index.php'>
-	<h2>Текст </h2>
-	<br>
-	<textarea name = 'text'></textarea>
-	<br><br>
-	<button type = 'submit' name = 'submit'>Отправить</button>
-	</form>
+	
+<div id="slideout">
+  <img src="img/nap.png" alt="Отправить отзыв" width = "10%" />
+  <div id="slideout_inner">
+    <? if ( isset($_SESSION['login']) and  isset($_SESSION['password'])) :?>
+		<div class = "form">
+			<form method = 'POST' action = 'index.php'>
+			<h2>Текст </h2>
+			<br>
+			<textarea name = 'text'></textarea>
+			<br><br>
+			<button type = 'submit' name = 'submit'>Отправить</button>
+			</form>
+		</div>
+	<? else :?>
+		<div class = "form">
+			<form method = 'POST' action = 'index.php'>
+			<h2>Логин </h2>
+			<input type = "text" name = "login">
+			<br><br>
+			<h2>Пароль </h2>
+			<input type = "text" name = "password">
+			<br><br>
+			<button type = 'submit' name = 'submit'>Войти</button> 
+			<br><br>
+			<button type = 'submit' name = 'submit'>Зарегистрироваться</button>
+			</form>
+		</div>
+	<? endif ;?>		
   </div>
+</div>
+	
+	
+  
 
   
   <footer class = 'footer'>
